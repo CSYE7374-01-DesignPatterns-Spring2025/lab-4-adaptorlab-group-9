553bdce49baa3bdfb77c3520424c8e1ab391ccb3
Adarsh Raj <raj.ada@northeastern.edu>
2025-02-04 19:50:40 -0500

    Add files via upload
    commit for lab4

---
+++ src/main/java/edu/neu/csye7374/AccumulatableAPI.java
@@ -0,0 +1,13 @@
+package edu.neu.csye7374;
+
+import java.util.List;
+
+public interface AccumulatableAPI {
+    double accumulation(List<Double> itemPrices);
+
+    double payment(double payment);
+
+    double multiply(double factor);
+
+    double divide(double divisor);
+}
